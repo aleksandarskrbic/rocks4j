ea1ad121d34a624bd07bc181dbd461e95590af82
Jovica Andric <jovica.andrickv@gmail.com>
2020-03-07 00:06:38 +0100

    Replaced jackson with kryo

--- src/main/java/com/github/aleksandarskrbic/rocksdb/mapper/RocksDBMapper.java
+++ src/main/java/com/github/aleksandarskrbic/rocksdb/mapper/RocksDBMapper.java
@@ -1,34 +1,44 @@
 package com.github.aleksandarskrbic.rocksdb.mapper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import com.github.aleksandarskrbic.rocksdb.exception.DeserializationException;
 import com.github.aleksandarskrbic.rocksdb.exception.SerializationException;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public final class RocksDBMapper<T> implements Mapper<T> {
 
     private final Class<T> type;
-    private final ObjectMapper mapper;
+    private final Kryo kryo;
 
     public RocksDBMapper(final Class<T> type) {
         this.type = type;
-        this.mapper = new ObjectMapper();
+        this.kryo = new Kryo();
+        kryo.register(type);
     }
 
     public byte[] serialize(final T t) throws SerializationException {
         try {
-            return mapper.writeValueAsBytes(t);
-        } catch (final JsonProcessingException exception) {
+            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            final Output output = new Output(byteArrayOutputStream);
+            kryo.writeObject(output, t);
+            output.flush();
+            output.close();
+            return byteArrayOutputStream.toByteArray();
+        } catch (final Exception exception) {
             throw new SerializationException(exception.getMessage());
         }
     }
 
     public T deserialize(final byte[] bytes) throws DeserializationException {
         try {
-            return mapper.readValue(bytes, type);
-        } catch (final IOException exception) {
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            final Input input = new Input(byteArrayInputStream);
+            return kryo.readObject(input, type);
+        } catch (final Exception exception) {
             throw new DeserializationException(exception.getMessage());
         }
     }
